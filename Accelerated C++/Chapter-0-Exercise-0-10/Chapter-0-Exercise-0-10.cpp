b4aee94e9987bef22103a5e60812d265dcd29887
LaVillaine <LaVillaine@users.noreply.github.com>
2018-01-29 11:54:00 +0000

    Exercise 0-10

---
+++ Accelerated C++/Chapter-0-Exercise-0-10/Chapter-0-Exercise-0-10.cpp
@@ -0,0 +1,18 @@
+// Chapter-0-Exercise-0-10.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int 
+main()
+{
+	std::cout
+		<<
+		"Hello, world!"
+		<<
+		std::endl;
+    return
+		0;
+}
+
