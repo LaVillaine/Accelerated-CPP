0d8810826f55a8fb230045b5d4d631a039f7c9a8
LaVillaine <LaVillaine@users.noreply.github.com>
2018-02-01 08:29:10 +0000

    added functionality for framing a name

--- Accelerated C++/Chapter1/Chapter1.cpp
+++ Accelerated C++/Chapter1/Chapter1.cpp
@@ -13,6 +13,21 @@ int main()
 	std::cin >> name;
 	std::cout << "Hello, " << name << "!" << std::endl;
 
+	// framing a name
+	std::cout << "Please enter your first name: ";
+	std::string firstName;
+	std::cin >> firstName;
+	const std::string greeting = "Hello, " + firstName + "!";
+	const std::string spaces(greeting.size(), ' ');
+	const std::string second = "* " + spaces + " *";
+	const std::string first(second.size(), '*');
+	std::cout << std::endl;
+	std::cout << first << std::endl;
+	std::cout << second << std::endl;
+	std::cout << "* " << greeting << " *" << std::endl;
+	std::cout << second << std::endl;
+	std::cout << first << std::endl;
+
     return 0;
 }
 
