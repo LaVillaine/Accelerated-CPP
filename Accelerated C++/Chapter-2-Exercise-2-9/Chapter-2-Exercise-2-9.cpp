1313dbda5d8f8d076caeb2bd8058897833478bbc
LaVillaine <LaVillaine@users.noreply.github.com>
2018-02-12 12:12:42 +0000

    implemented exercise 2-9

--- Accelerated C++/Chapter-2-Exercise-2-9/Chapter-2-Exercise-2-9.cpp
+++ Accelerated C++/Chapter-2-Exercise-2-9/Chapter-2-Exercise-2-9.cpp
@@ -2,10 +2,38 @@
 //
 
 #include "stdafx.h"
-
+#include <iostream>
+#include <string>
 
 int main()
 {
+	std::string firstNumStr;
+	std::string secondNumStr;
+
+	std::cout << "Enter the first integer: ";
+	std::cin >> firstNumStr;
+	if (firstNumStr.find_first_not_of("0123456789") != std::string::npos)
+	{
+		std::cout << "\nYou did not enter a valid integer. Goodbye." << std::endl;
+		return 0;
+	}
+
+	std::cout << "Enter the second integer: ";
+	std::cin >> secondNumStr;
+	if (secondNumStr.find_first_not_of("0123456789") != std::string::npos)
+	{
+		std::cout << "\nYou did not enter a valid integer. Goodbye." << std::endl;
+		return 0;
+	}
+
+	int firstNum = std::atoi(firstNumStr.c_str());
+	int secondNum = std::stoi(secondNumStr.c_str());
+	if (firstNum == secondNum)
+		std::cout << firstNumStr << " is equal to " << secondNumStr << std::endl;
+	else if(firstNum > secondNum)
+		std::cout << firstNumStr << " is greater than " << secondNumStr << std::endl;
+	else
+		std::cout << secondNumStr << " is greater than " << firstNumStr << std::endl;
     return 0;
 }
 
