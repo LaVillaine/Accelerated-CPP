dc2665108e79226336c6e8be9980a93cc8992c84
LaVillaine <LaVillaine@users.noreply.github.com>
2018-02-12 11:25:35 +0000

    implemented exercise 2-8

--- Accelerated C++/Chapter-2-Exercise-2-8/Chapter-2-Exercise-2-8.cpp
+++ Accelerated C++/Chapter-2-Exercise-2-8/Chapter-2-Exercise-2-8.cpp
@@ -2,10 +2,15 @@
 //
 
 #include "stdafx.h"
-
+#include <iostream>
 
 int main()
 {
+	// loop to produce 9 factorial
+	int product = 1;
+	for (int i = 2; i < 10; ++i)
+		product *= i;
+	std::cout << "\n 9 factorial is: "<<product << std::endl;
     return 0;
 }
 
