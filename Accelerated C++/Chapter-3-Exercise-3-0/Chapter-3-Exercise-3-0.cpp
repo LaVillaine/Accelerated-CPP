06b859baeb8a45fa66f03a085573e3073c5e4577
LaVillaine <LaVillaine@users.noreply.github.com>
2018-02-19 09:38:38 +0000

    implemented change to calculate median grade instead of average grade like the previous code, there is still a loophole where the user might enter more than 2 grades for midterm and final and the extra grades will be considered as part of the homework grade

--- Accelerated C++/Chapter-3-Exercise-3-0/Chapter-3-Exercise-3-0.cpp
+++ Accelerated C++/Chapter-3-Exercise-3-0/Chapter-3-Exercise-3-0.cpp
@@ -6,6 +6,8 @@
 #include <ios>
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 using std::cin;
 using std::cout;
@@ -13,6 +15,8 @@ using std::endl;
 using std::setprecision;
 using std::string;
 using std::streamsize;
+using std::vector;
+using std::sort;
 
 int main()
 {
@@ -27,30 +31,40 @@ int main()
 	double midterm, final;
 	cin >> midterm >> final;
 
-	// ask for the homework grades
+	// ask for and read the homework grades
 	cout << "Enter all your homework grades, "
 		"followed by end-of-file: ";
-
-	// the number and sum of grades read so far
-	int count = 0;
-	double sum = 0;
-
-	// a variable into which to read
+	vector<double> homework;
 	double x;
 
-	// invariant:
-	// we have read count grades so far and
-	// sum is the sum of the first count grades
+	// invariant: homework contains all the homework grades read so far
 	while (cin >> x)
+		homework.push_back(x);
+
+	// check that the student entered some homework grades
+	typedef vector<double>::size_type vec_sz;
+	vec_sz size = homework.size();
+	if (size == 0)
 	{
-		++count;
-		sum += x;
+		cout << endl << "You must enter your grades. "
+			"Please try again." << endl;
+		return 1;
 	}
 
-	// write the result
-	streamsize prec = cout.precision();
+	// sort the grades
+	sort(homework.begin(), homework.end());
+
+	// compute the median homework grade
+	vec_sz mid = size / 2;
+	double median;
+	median = size % 2 == 0 ? 
+		(homework[mid] + homework[mid - 1]) / 2
+		: homework[mid];
+
+	// compute and write the final grade
+    streamsize prec = cout.precision();
 	cout << "Your final grade is " << setprecision(3)
-		<< 0.2*midterm + 0.4*final + 0.4*sum / count
+		<< 0.2*midterm + 0.4*final + 0.4*median
 		<< setprecision(prec) << endl;
     return 0;
 }
